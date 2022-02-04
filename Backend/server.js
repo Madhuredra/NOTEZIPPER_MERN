e7dd20384d947385a848f2edd8266572bbe1b92c
Madhurendra Nath Tiwari <madhurendraakela123@gmail.com>
2022-02-04 14:25:39 +0530

    Push Backend and Frontend

---
+++ Backend/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const notes = require('./data/notes')
+const dotenv = require('dotenv');
+
+const app = express();
+dotenv.config();
+
+
+app.get('/',(req,res) => {
+    res.send("Hello Madhurendra");
+})
+
+app.get('/api/notes',(req,res) => {
+    res.json(notes)
+})
+
+app.get('/api/notes/:id',(req,res) => {
+    const note = notes.find((n)=>n._id===req.params.id);
+    // console.log(req.params);
+    res.send(note);
+})
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(5000,(req,res) => {
+    console.log(`Server is running on Port https://localhost:${PORT}`);
+})
---
+++ frontend/src/App.js
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Madhurendra Nath Tiwari</h1>
+    </div>
+  );
+}
+
+export default App;
